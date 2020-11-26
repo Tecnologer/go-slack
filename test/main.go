e01b9f7775c6788cdda4c5179436d67cf53d3c93
Rey David <rdominguez@tecnologer.net>
2020-11-25 23:19:35 -0600

    test: implement token from secrets

--- test/main.go
+++ test/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/sirupsen/logrus"
+	"github.com/tecnologer/go-secrets"
+	"github.com/tecnologer/go-secrets/config"
+	"github.com/tecnologer/go-slack/slack"
+	"github.com/tecnologer/go-slack/slack/models/response"
+)
+
+var s *slack.Slack
+
+func main() {
+	secrets.InitWithConfig(&config.Config{})
+	slackToken := secrets.GetKeyString("slack.token")
+
+	s = slack.New(slackToken)
+
+	err := s.SetCommand("hola", sayHi)
+	if err != nil {
+		logrus.WithError(err).Error("set command hola")
+	}
+
+	fmt.Println(s)
+}
+
+func sayHi(msg *response.Message) {
+	err := s.SendTextMessage("ULC4RM8HG", "hola desde go")
+	if err != nil {
+		logrus.WithError(err).Error("sending message")
+	}
 }
